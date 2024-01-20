1a25046b74020fb4adfb0082f1dee0b249d71495
José Nonato <beijallote@gmail.com>
2024-01-20 10:16:31 -0300

    feat: consuming github api

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,27 @@
-function App() {
+import { useEffect, useState } from 'react';
+
+export default function App() {
+  const [ repositorios, setRepositorios ] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Jose-Nonato/repos')
+    .then(response => response.json())
+    .then(data => setRepositorios(data))
+  }, [])
+
   return (
     <div>
-      <h1>Olá mundo!</h1>
+      <ul>
+        { repositorios.map(repositorio => {
+          return(
+            <li>
+              <h3>{repositorio.name}</h3>
+              <p>{repositorio.description}</p>
+              <a href={repositorio.html_url} target='_blank'>Saiba mais +</a>
+            </li>
+          )
+        }) }
+      </ul>
     </div>
   );
 }
-
-export default App;
